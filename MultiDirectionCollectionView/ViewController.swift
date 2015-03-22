62475fbf883690a103ec2294776ae18a6daa7516
Kyle Andrews <kandrews@credera.com>
2015-03-21 21:20:28 -0500

    Initial Commit

---
+++ MultiDirectionCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiDirectionCollectionView
+//
+//  Created by Kyle Andrews on 3/21/15.
+//  Copyright (c) 2015 Credera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
