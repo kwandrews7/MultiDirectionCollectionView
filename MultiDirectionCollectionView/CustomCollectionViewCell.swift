0036689bee4a39b6f3cc4026dac33e47ab58e205
Kyle Andrews <kandrews@credera.com>
2015-04-19 21:16:33 -0500

    Filling the collection view: Configuring the cell, the data source, and the delegate.

---
+++ MultiDirectionCollectionView/CustomCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CustomCollectionViewCell.swift
+//  MultiDirectionCollectionView
+//
+//  Created by Kyle Andrews on 3/22/15.
+//  Copyright (c) 2015 Credera. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    func setup() {
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.blackColor().CGColor
+        self.layer.cornerRadius = 5.0
+    }
+}
---
+++ MultiDirectionCollectionView/CustomCollectionViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CustomCollectionViewController.swift
+//  MultiDirectionCollectionView
+//
+//  Created by Kyle Andrews on 3/21/15.
+//  Copyright (c) 2015 Credera. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "customCell"
+
+class CustomCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 10
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 10
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CustomCollectionViewCell
+    
+        // Configure the cell
+        cell.label.text = "Sec \(indexPath.section)/Item \(indexPath.item)"
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+
+
+}
